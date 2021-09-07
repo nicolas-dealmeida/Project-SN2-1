534a521aedc9a3fbb9cfdc54fca7fd040418783a
kylian-duval <kduval@la-providence.net>
2021-09-07 11:47:18 +0200

    Create user.php

---
+++ class/user.php
@@ -0,0 +1,32 @@
+<?php
+class user
+{
+    //variable en priver
+
+    private $_id;
+    private $_nom;
+    private $_prenom;
+    private $_login;
+    private $_mdp;
+    private $_bdd;
+
+
+
+
+    //public
+
+
+    //fonction qui permet au user de se connecter elle attende en paramettre un login et un mdp
+    function login($login, $mdp)
+    {
+
+    }
+
+    //fonction qui permet au user de se register elle attende en commentaire un login, un mdp, nom et un prenom
+    function register($login, $mdp, $nom, $prenom)
+    {
+
+    }
+
+    
+}
