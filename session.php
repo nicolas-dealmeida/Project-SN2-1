a445cb2531021ebd8242a90763eacac8d91892fc
nicolas-dealmeida <ndealmeida@la-providence.net>
2021-09-13 15:14:20 +0200

    Patch Erreur.

--- session.php
+++ session.php
@@ -3,6 +3,7 @@
 
     try{
         $BDD = new PDO('mysql:host=192.168.64.50;dbname=GPS; charset=utf8', 'admin', 'root');
+        $BDD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }catch(Exception $e){
         die('Erreur : '. $e->getMessage());
     }
