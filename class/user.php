4091a04a04b5f41babbfdbaa17ac46da3578065c
nicolas-dealmeida <ndealmeida@la-providence.net>
2021-09-14 08:31:08 +0200

    Identation.

--- class/user.php
+++ class/user.php
@@ -1,55 +1,43 @@
 <?php
-class user
-{
+class user{
     // Variable privée
-
-    private $_id;
-    private $_nom;
-    private $_prenom;
-    private $_login;
-    private $_mdp;
-    private $_BDD;
-
+        private $_id;
+        private $_nom;
+        private $_prenom;
+        private $_login;
+        private $_mdp;
+        private $_BDD;
     // Variable public
-
-    function __construct($BDD)
-    {
-        $this->_BDD = $BDD;
-    }
-
+        function __construct($BDD){
+            $this->_BDD = $BDD;
+        }
     // Fonction qui permet au user de se connecter, elle attend en paramétre un login et un mdp
-    function connexion($login, $mdp)
-    {
-        $requser = $this->_BDD->prepare("SELECT * FROM `user` WHERE `pseudo` = ? AND `mdp` = ?");
-        $requser->execute(array($login, $mdp));
-        $userexist = $requser->rowCount();
-        if ($userexist == 1) {
-            $userexist = $requser->fetch();
-            $_SESSION['id_user'] = $userexist['id'];
-           
-        }else{
-            echo "erreur mdp incorecte";
+        function connexion($login, $mdp){
+            $requser = $this->_BDD->prepare("SELECT * FROM `user` WHERE `pseudo` = ? AND `mdp` = ?");
+            $requser->execute(array($login, $mdp));
+            $userexist = $requser->rowCount();
+            if ($userexist == 1) {
+                $userexist = $requser->fetch();
+                $_SESSION['id_user'] = $userexist['id'];
+            }else{
+                $MSG = "Mots de passe incorrect.";
+            }
         }
-    }
-
-    function deconnexion()
-    {
-        session_destroy();
-        include("connexion.php");
-        exit();
-    }
-
+    // Fonction qui permet au user de se déconnecter
+        function deconnexion(){
+            session_destroy();
+            include("connexion.php");
+            exit();
+        }
     // Fonction qui permet au user de s'inscrire, elle attend en commentaire un login, un mdp, un nom et un prénom
-    function inscription($login, $mdp, $nom, $prenom, $confmdp)
-    {
-        if ($mdp == $confmdp) {
-
-            $req = "INSERT INTO `user`(`nom`, `prenom`, `pseudo`, `mdp`, `admin`) VALUES ('$nom', '$prenom', '$login', '$mdp','0')";
-            $RequetStatement = $this->_BDD->query($req);
-        } else {
-            echo "erreur";
+    function inscription($login, $mdp, $nom, $prenom, $confmdp){
+        if($mdp == $confmdp){
+            $req = "INSERT INTO `user`(`nom`, `prenom`, `pseudo`, `mdp`) VALUES ('$nom', '$prenom', '$login', '$mdp')";
+            $this->_BDD->query($req);
+            $MSG = "Inscription réussite.";
         }
-
-        //include("index.php");
+        else{
+            $MSG = "Le Mots de passe n'est pas le même.";
+        }
     }
 }
