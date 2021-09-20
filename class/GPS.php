f2a34ffe149bc5c068ec8c417ac5d137bbc01bc9
kylian-duval <kduval@la-providence.net>
2021-09-20 19:14:19 +0200

    update

--- class/GPS.php
+++ class/GPS.php
@@ -2,7 +2,8 @@
     class GPS{
         //Private
             private $_id;
-            private $_nom;
+            private $_nomBateau;
+            private $_idBateau;
             private $_latitude;
             private $_longitude;
             private $_bdd;
@@ -11,19 +12,33 @@
                 $this->_bdd = $BDD;
             }
         //Fonction qui permet d'ajouter des coordonnées en base de données elle attend en paramètre un nom, latitude et la longitude
-            public function addcoordonnees($nom, $latitude, $longitude){
-                $reqaddcoordonnees = $this->_BDD->prepare("INSERT INTO `gps`(`id_bateau`, `latitude`, `longitude`) VALUES ('$nom','$latitude','$longitude')");
-                $reqaddcoordonnees->execute(array($nom, $latitude, $longitude));
+            public function addcoordonnees($latitude, $longitude){
+                
+                $reqaddcoordonnees = $this->_BDD->query("INSERT INTO `gps`(`id_bateau`, `latitude`, `longitude`) VALUES ('$this->_idBateau','$latitude','$longitude')");
             }
-        //Fonction qui permet de mettre à jour les coordonnées GPS elle attend en paramètre le nom du point gps
-            public function updatecoordonnees($nom){
-                $requpdatecoordonnees = $this->_BDD->prepare("UPDATE `gps` SET `id_bateau`='$nom'");
+            
+            //function qui permet de récuper l'id du bateau elle prend en parametre le nom du bateau et return rien
+            public function getbateau($nom){
+                $idbateau = $this->_bdd->query("SELECT `id`, `nom` FROM `bateau` WHERE `nom` = '$nom'");
+                $data = $idbateau->fetch();
+                $this->_idBateau = $data["nom"];
+                $this->__nomBateau = $data["id"];
+            }
+
+        //Fonction qui permet de mettre à jour les coordonnées GPS elle attend en paramètre latitude et la longitude
+            public function updatecoordonnees($latitude, $longitude){
+                $requpdatecoordonnees = $this->_BDD->prepare("UPDATE `gps` SET `latitude`='$latitude',`longitude`='$longitude' WHERE `id_bateau` ='$this->_idBateau'");
                 $requpdatecoordonnees->execute();
             }
         //Fonction qui permet de supprimer un point gps de la base de données elle attend en paramètre le nom du point gps
             public function removeGPS($nom){
                 $reqremoveGPS = $this->_BDD->prepare("DELETE FROM `gps` WHERE `id_bateau`='$nom'");
                 $reqremoveGPS->execute();
+            }
+
+            //function qui permet de suprimer des bateau en base elle attend en parametre l'id du bateau
+            public function removeBateau($id){
+                $this->_bdd->query("DELETE FROM `bateau` WHERE `id` ='$id'");
             }
         //Fonction qui donne les coordonnées d'un point gps elle attend en paramètre le nom du point gps
             public function getcoordonnees($nom){
