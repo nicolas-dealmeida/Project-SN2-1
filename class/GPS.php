606813f4e4a3bd75e6899689f352866fb32d6416
kylian-duval <kduval@la-providence.net>
2021-09-20 20:11:55 +0200

    update

--- class/GPS.php
+++ class/GPS.php
@@ -13,16 +13,15 @@
             }
         //Fonction qui permet d'ajouter des coordonnées en base de données elle attend en paramètre un nom, latitude et la longitude
             public function addcoordonnees($latitude, $longitude){
-                
-                $reqaddcoordonnees = $this->_BDD->query("INSERT INTO `gps`(`id_bateau`, `latitude`, `longitude`) VALUES ('$this->_idBateau','$latitude','$longitude')");
+                $this->_BDD->query("INSERT INTO `gps`(`id_bateau`, `latitude`, `longitude`) VALUES ('$this->_idBateau','$latitude','$longitude')");
             }
             
             //function qui permet de récuper l'id du bateau elle prend en parametre le nom du bateau et return rien
             public function getbateau($nom){
                 $idbateau = $this->_bdd->query("SELECT `id`, `nom` FROM `bateau` WHERE `nom` = '$nom'");
                 $data = $idbateau->fetch();
-                $this->_idBateau = $data["nom"];
-                $this->__nomBateau = $data["id"];
+                $this->_idBateau = $data["id"];
+                $this->_nomBateau = $data["nom"];
             }
 
         //Fonction qui permet de mettre à jour les coordonnées GPS elle attend en paramètre latitude et la longitude
@@ -41,9 +40,12 @@
                 $this->_bdd->query("DELETE FROM `bateau` WHERE `id` ='$id'");
             }
         //Fonction qui donne les coordonnées d'un point gps elle attend en paramètre le nom du point gps
-            public function getcoordonnees($nom){
-                $reqgetcoordonnees = $this->_BDD->prepare("SELECT `id_bateau` FROM `gps`");
+            public function getcoordonnees(){
+                $reqgetcoordonnees = $this->_BDD->prepare("SELECT * FROM `gps` WHERE `id_bateau` ='$this->_idBateau'");
                 $reqgetcoordonnees->execute();
+                $data = $reqgetcoordonnees->fetch();
+                $this->_latitude = $data['latitude'];
+                $this->_longitude = $data['longitude'];
             }
         //Fonction qui retourne latitude
             public function getlatitude(){
